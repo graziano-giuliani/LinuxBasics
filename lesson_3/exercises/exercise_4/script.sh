26f16402391ba09b43bd2a005806c3bf92773977
Graziano Giuliani <ggiulian@ictp.it>
2022-08-10 19:04:17 +0200

    complete lessons review. Still lacks python part.

--- lesson_3/exercises/exercise_4/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# After the file is sorted according to the 3rd column, this column will assume
-# its largest value at the last row (the first, if you sort with the '-r'
-# option). I select this row with 'tail -n1' and from it its second field.
-paste -d' ' example_file_1 example_file_2 | sort -k3 -g | tail -n1 | cut -d' ' -f2
-
-# Swapping the last 2 commands is also correct, but less efficient.
-#paste -d' ' example_file_1 example_file_2 | sort -k3 -g | cut -d' ' -f2 | tail -n1
